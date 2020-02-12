cd4445c86f459a9d146c7e49640bbea5080120b2
JuanTPB <54864553+JuanTPB@users.noreply.github.com>
2020-02-11 18:19:54 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-<html>
-<head>
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <iframe src="https://player.twitch.tv/?channel=renrize" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/renrize?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from RenRize on www.twitch.tv</a>
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
 
-<iframe src="https://player.twitch.tv/?channel=renrize" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/renrize?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from RenRize on www.twitch.tv</a>
-  </div>
-
-</body>
-</html>
+export default App;
