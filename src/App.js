05497e70e77add6c29d0ea0d44617e602fff78af
JuanTPB <54864553+JuanTPB@users.noreply.github.com>
2020-02-11 18:16:57 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 <html>
 <head>
-<meta charset=”utf-8″>
 
-<section class="stream-sec" id="home" style="box-shadow: -2px 6px 98px -6px rgba(0,0,0,0.75); background: url(../img/bg-01.jpg) no-repeat;  background-size: cover;">
-  <br>
-  <br>
-  <br>
-  <div class="container form-block">
-    <div id="myDIV" class="row" style="box-shadow: -2px 6px 98px -6px rgba(0,0,0,0.75); border:5px solid #008ae6; border-radius: 9px 9px 9px 9px">
-    	
-      <div class='embed-container  col-md-9 col-lg-9 content-sec'><iframe src='https://player.twitch.tv/?channel=renrize' height='720' width='1280' frameborder='0' scrolling='no' allowfullscreen='true'></iframe></div> <div class='embed-containerchat  col-md-3 col-lg-3 content-sec'> <iframe style='' frameborder='0' scrolling='no' id='chat_embed' src='https://www.twitch.tv/embed/renrize/chat' height='720' width='290'> </iframe> </div> <div class="hidden-xs"><button class='btn btn-primary' style='position: absolute; right: 9px; top: 9px;'onclick='myFunction()'>OCULTAR CHAT</button></div>
-      
-    </div>
+
+<iframe src="https://player.twitch.tv/?channel=renrize" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe><a href="https://www.twitch.tv/renrize?tt_content=text_link&tt_medium=live_embed" style="padding:2px 0px 4px; display:block; width:345px; font-weight:normal; font-size:10px; text-decoration:underline;">Watch live video from RenRize on www.twitch.tv</a>
   </div>
 
 </body>
